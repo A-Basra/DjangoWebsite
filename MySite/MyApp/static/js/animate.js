22712417d437dd128a781860cb894bf6d8b61241
A-Basra <82539141+A-Basra@users.noreply.github.com>
2023-02-07 10:36:07 +0000

    updated

--- MySite/MyApp/static/js/animate.js
+++ MySite/MyApp/static/js/animate.js
@@ -1,16 +1,25 @@
-var moved = false;
+
 function move() {
-    while (!moved){
-        var logo = document.getElementById("Logo");
-            logo.animate([
-            { 'left': '50vw' , 'top': '25vh'},
-            { 'left': '5vw', 'top':'7.5vh'}
-            ],
-            { duration: 1000});
-            logo.style.left = "5vw"
-            logo.style.top = "7.5vh"
-            moved = true;
-        }
+    var logo = document.getElementById("Logo");
+    var menu = document.getElementById("Menu");
+
+    logo.animate([
+    { 'left': '50vw' , 'top': '25vh'},
+    { 'left': '5vw', 'top':'7.5vh'}
+    ],
+    { duration: 1000});
+
+    menu.animate([
+        {'left' : '50vw', 'top' :'40vh', 'width': '10vw'},
+        {'left' : '0vw', 'top' :'5vh', 'width': '7.5vw'}
+    ],
+    {duration: 1000});
+
+    logo.style.left = "5vw"
+    logo.style.top = "7.5vh"
+    menu.style.left = '0vw'
+    menu.style.top = '5vh'
+    menu.style.width= '7.5vw'
     return true;
 }
 expanded =false
