2a465aecb076efb4e4e557ca09b220bf9b7bcc80
A-Basra <82539141+A-Basra@users.noreply.github.com>
2023-02-07 01:45:54 +0000

    PageLayouts

--- MySite/MyApp/static/js/animate.js
+++ MySite/MyApp/static/js/animate.js
@@ -1,9 +1,28 @@
 var moved = false;
 function move() {
-    var logo = document.getElementById("Logo");
-    var position = 0;
-    if (!moved) {
-        logo.animate({ 'left': '0px' }, { duration, 400});
-        moved = true;
+    while (!moved){
+        var logo = document.getElementById("Logo");
+            logo.animate([
+            { 'left': '50vw' , 'top': '25vh'},
+            { 'left': '5vw', 'top':'7.5vh'}
+            ],
+            { duration: 1000});
+            logo.style.left = "5vw"
+            logo.style.top = "7.5vh"
+            moved = true;
+        }
+    return true;
+}
+expanded =false
+function expandBg(){
+    var bg = document.getElementById("background");
+    if (!expanded) {
+        bg.animate([
+        { 'width': '1vw'},
+        { 'width': '90vw'}
+        ],
+        { duration: 1000});
+        bg.style.width = "90vw"
+        expanded = true;
     }
 }
